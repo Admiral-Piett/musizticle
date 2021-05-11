0019a43189594625973a62f55d54995d31e7dc68
Devin Humphreys <devinhumphreys@hotmail.com>
2021-05-10 22:43:21 -0400

    Experiment with embedding a postgres db in here

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/Admiral-Piett/sound_control
 go 1.15
 
 require (
+	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/fergusstrange/embedded-postgres v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.3
+	github.com/klauspost/compress v1.12.2 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/lib/pq v1.10.1
+	github.com/pierrec/lz4/v4 v4.1.6 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 )
--- src/app.go
+++ src/app.go
@@ -1,9 +1,17 @@
 package src
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/Admiral-Piett/sound_control/src/daos"
 	"github.com/Admiral-Piett/sound_control/src/handlers"
+	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
 	"github.com/gorilla/mux"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"time"
 )
 
 type App struct {
@@ -12,6 +20,8 @@ type App struct {
 }
 
 func New() *App {
+	initializePostgres()
+
 	appDaos := daos.InitializeDaos()
 	defer appDaos.CloseAllDaos()
 	appHandlers := handlers.InitializeHandlers(appDaos)
@@ -27,4 +37,35 @@ func New() *App {
 func (a *App) initRoutes() {
 	a.Router.HandleFunc("/", a.Handlers.IndexHandler.Index()).Methods("GET")
 	a.Router.HandleFunc("/artists", a.Handlers.ArtistsHandler.Artists()).Methods("GET", "POST")
+}
+
+func initializePostgres() {
+	_, scriptPath, _, _ := runtime.Caller(0)
+	directory := filepath.Join(filepath.Dir(scriptPath), "..")
+	runtimePath := directory + "/postgres/runtime"
+	dataPath := directory + "/postgres/data"
+	os.MkdirAll(runtimePath, 0755)
+	os.MkdirAll(dataPath, 0755)
+
+	fmt.Println(directory)
+	// Start up the embedded postgres instance
+	logger := &bytes.Buffer{}
+	port, _ := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	postgres := embeddedpostgres.NewDatabase(embeddedpostgres.DefaultConfig().
+		Username(os.Getenv("POSTGRES_USER")).
+		Password(os.Getenv("POSTGRES_PASSWORD")).
+		Database(os.Getenv("POSTGRES_DB")).
+		Version(embeddedpostgres.V12).
+		RuntimePath(fmt.Sprintf("")).
+		DataPath(dataPath).
+		Port(uint32(port)).
+		StartTimeout(10 * time.Second).
+		Logger(logger))
+	go func() {
+		err := postgres.Start()
+		defer postgres.Stop()
+		if err != nil {
+			panic(err)
+		}
+	}()
 }
