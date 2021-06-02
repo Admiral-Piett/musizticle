a4670147010dd68cd3e69ebae4dc60dd009ac563
Devin Humphreys <devinhumphreys@hotmail.com>
2021-06-01 22:55:53 -0400

    Attempt with straight react

---
+++ web/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is Boilerplate Project</p>, document.getElementById('app'));
